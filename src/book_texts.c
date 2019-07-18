26e67f1ab803f1ad882d462c869ddd066214ee8b
tipsypastels <dakotasankey@gmail.com>
2019-07-18 11:47:31 -0700

    longer test msg

--- src/book_texts.c
+++ src/book_texts.c
@@ -1,6 +1,6 @@
 #include "global.h"
 
-static const u8 gBook_Test[] = _("TEST BOOK TEXT");
+static const u8 gBook_Test[] = _("And the Nonman King cried words that string:\nNow to me you must confess,\nFor death above you hovers!\nAnd the Emissary answered ever wary:\nWe are the race of flesh,\nWe are the race of lovers.");
 
 static const u8 *const gBookList[] = {
     gBook_Test,
@@ -10,5 +10,5 @@ const u8 *const getBook(u8 index)
 {
     if (index < sizeof(gBookList) / sizeof(gBookList[0]))
         return gBookList[index];
-    return "Bad book index :/"; 
+    return "Bad book index :/";
 }
