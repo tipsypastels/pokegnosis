7f2563a873b8a2329351459d3ef5a2949a1ad86d
tipsypastels <dakotasankey@gmail.com>
2019-09-29 02:19:51 -0700

    more mapping

--- src/new_game.c
+++ src/new_game.c
@@ -128,7 +128,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(LITTLEROOT_TOWN_MAYS_HOUSE_2F), MAP_NUM(LITTLEROOT_TOWN_MAYS_HOUSE_2F), -1, -1, -1);
+    SetWarpDestination(MAP_GROUP(OLDALE_TOWN), MAP_NUM(OLDALE_TOWN), -1, -1, -1);
     WarpIntoMap();
 }
 
