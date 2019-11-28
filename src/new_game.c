3df886bd5ae900665ad362bba40eed721d71abeb
tipsypastels <dakotasankey@gmail.com>
2019-11-28 10:52:49 -0800

    old lady

--- src/new_game.c
+++ src/new_game.c
@@ -94,7 +94,7 @@ static void InitPlayerTrainerId(void)
 // L=A isnt set here for some reason.
 static void SetDefaultOptions(void)
 {
-    gSaveBlock2Ptr->optionsTextSpeed = OPTIONS_TEXT_SPEED_MID;
+    gSaveBlock2Ptr->optionsTextSpeed = OPTIONS_TEXT_SPEED_FAST;
     gSaveBlock2Ptr->optionsWindowFrameType = 0;
     gSaveBlock2Ptr->optionsSound = OPTIONS_SOUND_MONO;
     gSaveBlock2Ptr->optionsBattleStyle = OPTIONS_BATTLE_STYLE_SHIFT;
@@ -133,7 +133,7 @@ static void WarpToTruck(void)
 
     // SetWarpDestination(MAP_GROUP(PETALBURG_WOODS), MAP_NUM(PETALBURG_WOODS), -1, -1, -1);
     // SetWarpDestination(MAP_GROUP(ROUTE103), MAP_NUM(ROUTE103), -1, -1, -1);
-    SetWarpDestination(MAP_GROUP(LITTLEROOT_TOWN_BRENDANS_HOUSE_2F), MAP_NUM(LITTLEROOT_TOWN_BRENDANS_HOUSE_2F), -1, -1, -1);
+    SetWarpDestination(MAP_GROUP(LITTLEROOT_TOWN_MAYS_HOUSE_2F), MAP_NUM(LITTLEROOT_TOWN_MAYS_HOUSE_2F), -1, -1, -1);
     // SetWarpDestination(MAP_GROUP(ROUTE105), MAP_NUM(ROUTE105), 20, 12, -1);
 
     WarpIntoMap();
